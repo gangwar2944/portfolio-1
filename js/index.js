ad0e7db40b9dc6e23121583f7c425a125134c0a2
sonu yadav <7398sonuyadav@gmail.com>
2021-05-26 21:54:16 +0530

    new design

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-
+var text = document.getElementById("text");
+var shadow = "";
+for (var i = 0; i < 15; i++) {
+  shadow += (shadow ? "," : "") + -i * 1 + "px " + i * 1 + "px 0 #d9d9d9";
+}
+text.style.textShadow = shadow;
