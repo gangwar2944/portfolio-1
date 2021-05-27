0ed8908aba3957b691410193a7295277bb2882bd
sonu yadav <7398sonuyadav@gmail.com>
2021-05-27 22:36:06 +0530

    just checking

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@
 let text = document.getElementById("text");
 let descBox = document.querySelector('#desc-box');
 let description  = document.querySelector('#desc');
+let navLinks =document.querySelectorAll('.side-nav a');
+
+//animating introduction
+
+window.onload = () =>{
 setTimeout(() => {
 let shadow = "";
   for (var i = 0; i < 15; i++) {
@@ -9,12 +14,17 @@ let shadow = "";
   }
   text.style.textShadow = shadow;
 },500);
-setTimeout(() => {
+let id = setTimeout(() => {
   if(window.screen.width < 992){
     text.style.transform='scale(.4) rotate(-30deg) skew(25deg) translateX(20px)';
   }else{
     text.style.transform='scale(.6) rotate(-30deg) skew(25deg) translateX(20px)';
   }
 },2000);
-//animating introduction
-
+if(id){
+  setTimeout(() => {
+    descBox.style.transform = 'translateX(0px)';
+    navLinks[0].classList.add('active');
+},2000);
+}
+}
