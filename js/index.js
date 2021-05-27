80002ce5ecca3666d506a8ad8e5c98356728c644
sonu yadav <7398sonuyadav@gmail.com>
2021-05-27 23:06:44 +0530

    animating

--- js/index.js
+++ js/index.js
@@ -3,10 +3,11 @@ let text = document.getElementById("text");
 let descBox = document.querySelector('#desc-box');
 let description  = document.querySelector('#desc');
 let navLinks =document.querySelectorAll('.side-nav a');
+let  aboutme = document.querySelector('.aboutme');
 
 //animating introduction
 
-window.onload = () =>{
+window.onload = () => {
 setTimeout(() => {
 let shadow = "";
   for (var i = 0; i < 15; i++) {
@@ -14,17 +15,24 @@ let shadow = "";
   }
   text.style.textShadow = shadow;
 },500);
-let id = setTimeout(() => {
+let ids = setTimeout(() => {
   if(window.screen.width < 992){
     text.style.transform='scale(.4) rotate(-30deg) skew(25deg) translateX(20px)';
   }else{
     text.style.transform='scale(.6) rotate(-30deg) skew(25deg) translateX(20px)';
   }
 },2000);
-if(id){
-  setTimeout(() => {
+setTimeout(() => {
     descBox.style.transform = 'translateX(0px)';
     navLinks[0].classList.add('active');
+    navLinks[5].classList.add('active');
+
 },2000);
-}
+setTimeout(() => {
+    aboutme.classList.add('animateAbout');
+},3000);
+
+
+
+
 }
