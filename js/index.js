75adcdb0c13bacc24ca60e8d99c774d65ae2a981
sonu yadav <7398sonuyadav@gmail.com>
2021-06-04 16:22:40 +0530

    updating home page

--- js/index.js
+++ js/index.js
@@ -2,15 +2,16 @@
 let text = document.getElementById("text");
 let descBox = document.querySelector('#desc-box');
 let description  = document.querySelector('#desc');
-let navLinks =document.querySelectorAll('.side-nav a');
+let smallNavLinks =document.querySelectorAll('#small-nav a');
+let largeNavLinks =document.querySelectorAll('#large-nav a');
 let  aboutme = document.querySelector('.aboutme');
 let  hi = document.querySelector('#hi');
 let  iam = document.querySelector('#iam');
 
 
-//animating introduction
-
+//animating introduction on page load
 window.onload = () => {
+  //for text shadow animation start "sonu yadav large in background"
 setTimeout(() => {
 let shadow = "";
   for (var i = 0; i < 15; i++) {
@@ -25,10 +26,12 @@ let ids = setTimeout(() => {
     text.style.transform='scale(.6) rotate(-30deg) skew(25deg) translateX(20px)';
   }
 },2000);
+//for text shadow animation end
+// activating about nav pannel
 setTimeout(() => {
     descBox.style.transform = 'translateX(0px)';
-    navLinks[0].classList.add('active');
-    navLinks[5].classList.add('active');
+    smallNavLinks[0].classList.add('active');
+    largeNavLinks[0].classList.add('active');
 
 },2000);
 setTimeout(() => {
@@ -42,6 +45,8 @@ setTimeout(() => {
 },3000);
 
 }
+//animating introduction on page load end here
+//changing text and color after hi
 var i =0;
 function changeText(){
   let tags = document.querySelector('#tags');
@@ -53,7 +58,7 @@ i++;
 if( i === tags_data.length){
   i =0;
 }
-
 }
 changeText();
 setInterval(changeText,1000);
+// changing text end here
