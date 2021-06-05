e8927cd8c09026b8974e3555cc024b14e7986f0b
sonu yadav <7398sonuyadav@gmail.com>
2021-06-05 11:40:25 +0530

    changing about text

--- js/index.js
+++ js/index.js
@@ -64,5 +64,29 @@ changeText();
 setInterval(changeText,1000);
 // changing text end here
 
-//activating clicked navbar
+//animatiing background
+// window.addEventListener('DOMContentLoaded',setup);
+// function setup(){
+//   function createDots(){
+//   const canvas = document.getElementById('bg-animate');
+//    const width = canvas.offsetWidth;
+//    const height = canvas.offsetHeight;
+//   const classes =['colour-1','colour-2','colour-3','colour-4','colour-5'];
 
+//  for(let i =0; i < width; i+=20){
+//    for(let j=0; j<height; j+=20){
+//   const outerDiv = document.createElement('div');
+// outerDiv.classList.add('bg-box');
+// const innerDiv =document.createElement('div');
+// innerDiv.classList.add('dot');
+
+// let random = Math.floor(Math.random() * 5);
+// innerDiv.classList.add(classes[random]);
+// outerDiv.appendChild(innerDiv);
+// canvas.appendChild(outerDiv);
+//    }
+//  }
+
+//   }
+//   createDots();
+// }
